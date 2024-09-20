6b1eb66ef24ab930d7dc3f0c72c402be5d977061
chaudharysagar <sagarchaudharynn06@gmail.com>
2024-09-21 00:11:01 +0530

    welcome page page added

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,14 +1,96 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Pressable } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import SildeShow from '../../components/SlideShow';
+
 
 const WelcomePage = () => {
-  return (
-    <View>
-      <Text>WelcomePage</Text>
-    </View>
-  )
-}
+   return (
+      <View style={styles.container}>
+         <ImageBackground
+            source={require('./../../assets/images/welcome.jpeg')}
+            style={styles.backgroundImage}
+            resizeMode="cover"
+         >
+            <LinearGradient
+               colors={['rgba(0,0,0,0.6)', 'rgba(0,0,0,0.4)', 'rgba(0,0,0,0.6)']}
+               style={styles.gradient}
+            />
 
-export default WelcomePage
+            <View style={styles.welcome}>
 
-const styles = StyleSheet.create({})
+               <View style={{ marginTop: 10 }}>
+                  <Text style={[styles.text, styles.heading]}>Welcome to WATERX</Text>
+                  <Text style={[styles.text, styles.slogan]} >Every Drop Counts, Every Idea Creates Change</Text>
+               </View>
+               <View>
+               <Text style={styles.secondHeading}>What You Can Do Here</Text>
+               <SildeShow></SildeShow>
+               </View>
+               <Pressable style={{
+                  padding: 14,
+                  width: '90%',
+                  backgroundColor: "#94D8DB",
+                  borderRadius: 14,
+                  marginTop:60
+               }}><Text style={styles.btn}>Explore More</Text></Pressable>
+            </View>
+         </ImageBackground>
+      </View>
+   );
+};
+
+export default WelcomePage;
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      fontFamily: "outfit",
+   },
+   backgroundImage: {
+      flex: 1,
+   },
+   gradient: {
+      ...StyleSheet.absoluteFillObject,
+   },
+   heading: {
+      fontSize: 26,
+      fontFamily: 'outfit-bold',
+      textAlign: 'center',
+      color: '#94D8DB'
+   },
+   welcome: {
+      marginTop:100,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 50
+
+   },
+   slogan: {
+      textAlign: 'center',
+      fontSize: 15,
+      fontFamily: 'outfit',
+      color: '#DDC7A0'
+   },
+   welcomePara: {
+      lineHeight: 22,
+      fontSize: 17,
+      color: '#94D8DB',
+      fontFamily: 'outfit',
+      textAlign: 'center'
+   },
+   btn: {
+      textAlign: 'center',
+      fontFamily: 'outfit-medium',
+      fontSize: 20
+   },
+   secondHeading:{
+      color:'white',
+      fontFamily:'outfit-bold',
+      fontSize:20,
+      textAlign:'center',
+      marginBottom:10
+   },
+
+});
